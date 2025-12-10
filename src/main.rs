7f07ff6b3af4173577c50fa5b8eeb470d0770010
pi826 <fefr628@gmail.com>
2025-12-10 21:26:28 +0900

    main関数に文字列フォーマットの例を追加

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,11 @@ fn add(a: i32, b: i32) -> i32 {
 
 fn main() {
     println!("{}", add(5, 3));
+
+    println!("Hello {:20}!", "world");  // 幅20（固定値）で"world"を表示
+    println!("Hello {:1$}!", "Rust", 20);  // 幅を第2引数(20)で指定して"Rust"を表示
+    println!("Hello {1:0$}!", 20, "Rust"); // "Rust"を第1引数の幅(20)で表示
+    println!("Hello {2:0$}!", 20, "ignored", "Rust"); // "Rust"を第1引数の幅(20)で表示
+    println!("Hello {2:1$}!", "ignored", 20, "Rust"); // "Rust"を第2引数の幅(20)で表示
+
 }
